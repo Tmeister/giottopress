ca0259eec6b521015f96b1507e08476096a73089
Enrique Chavez <noone@tmeister.net>
2017-07-21 14:31:32 -0500

    Variables

--- footer.php
+++ footer.php
@@ -15,22 +15,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-</div><!-- #wrapper -->
-</div><!-- #content -->
+        </div><!-- #wrapper -->
+    </div><!-- #content -->
 </div><!-- #page -->
 
 <?php do_action( 'giotto/before_footer' ); ?>
-
-<footer id="colophon" class="site-footer">
-<!--	--><?php
-//	do_action( 'giotto/before_footer_content' );
-//	do_action( 'giotto/footer' );
-//	do_action( 'giotto/after_footer_content' );
-//	?>
-    Copy
-</footer><!-- #footer -->
-
+<?php do_action( 'giotto/footer' ) ?>
+<?php do_action( 'giotto/after_footer' ); ?>
 <?php wp_footer(); ?>
+<div class="is-clearfix"></div>
 
-</body>
-</html>
+</body><!-- body -->
+</html><!-- html -->
